2676f4c2e8c730a858d3b54b51ac3a080347a454
Soley Pasban <pasban110@gmail.com>
2019-05-19 01:12:16 +0300

    add money view added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 import history from './dictionary/History';
 import { AccountsView } from './views/AccountsView';
 import { LoginView } from './views/LoginView';
 import { ExchangeView } from './views/ExchangeView';
+import { AddMoneyView } from './views/AddMoneyView';
 
-function App() {
-  return (
-    <div className="r-app noselect">
-      <Router history={history}>
-        <Route path="/" exact component={LoginView} />
-        <Route path="/accounts" exact component={AccountsView} />
-        <Route path="/exchange" exact component={ExchangeView} />
-      </Router>
-    </div>
-  );
-}
+// const search = location.search;
+// const params = new URLSearchParams(search);
+// const foo = params.get('foo');
+
+const App = ({location}) => 
+  <div className="r-app noselect">
+   <Router history={history}>
+    <Route  path="/" exact component={LoginView} />
+    <Route path="/accounts" component={AccountsView} />
+    <Route path="/exchange/:from?/:to?" component={ExchangeView} />
+    <Route path="/add" component={AddMoneyView} />
+   </Router>
+  </div>
 
 export default App;
---
+++ src/views/AddMoneyView.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BalancesActionController } from '../controllers/BalancesActionController';
+import { TransactionsListController } from '../controllers/TransactionsListController';
+
+const AddMoneyView = props =>
+    <div className='r-view'>
+        AddMoneyView
+    </div>
+
+export { AddMoneyView };
