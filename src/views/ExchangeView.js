bdb644ab2570b69da33015c7f67f5f72811c84b0
Soley Pasban <pasban110@gmail.com>
2019-05-19 00:44:17 +0300

    exchange view added

---
+++ src/views/ExchangeView.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BalancesActionController } from '../controllers/BalancesActionController';
+import { TransactionsListController } from '../controllers/TransactionsListController';
+
+const ExchangeView = props =>
+    <div className='r-accounts-view'>
+        ExchangeView
+    </div>
+
+export { ExchangeView };
