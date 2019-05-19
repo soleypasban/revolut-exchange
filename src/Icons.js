7a763beb7e7e9d6e4547dd7d4dfbad2d69e17cc1
Soley Pasban <pasban110@gmail.com>
2019-05-19 10:54:40 +0300

    swap icon added

--- src/Icons.js
+++ src/Icons.js
@@ -46,11 +46,21 @@ export const arrowDown =
             c12.365,12.354,12.365,32.392,0,44.751L248.292,345.449C242.115,351.621,234.018,354.706,225.923,354.706z"/>
     </svg>
 
+export const swap =
+    <svg viewBox="0 0 490 490">
+        <polygon fill="currentColor" points="85.877,154.014 85.877,428.309 131.706,428.309 131.706,154.014 180.497,221.213 217.584,194.27 108.792,44.46 
+            0,194.27 37.087,221.213 	"/>
+        <polygon fill="currentColor" points="404.13,335.988 404.13,61.691 358.301,61.691 358.301,335.99 309.503,268.787 272.416,295.73 381.216,445.54 
+            490,295.715 452.913,268.802 	"/>
+    </svg>
+
+
 export const Icons = {
     plus,
     exchange,
     details,
     close,
     arrowLeft,
-    arrowDown
+    arrowDown,
+    swap
 }
