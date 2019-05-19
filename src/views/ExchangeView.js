576ec397e4db40368a7914edaa7285b983906cb1
Soley Pasban <pasban110@gmail.com>
2019-05-19 10:19:03 +0300

    exchange layout added

--- src/views/ExchangeView.js
+++ src/views/ExchangeView.js
@@ -5,6 +5,16 @@ const ExchangeView = ({ match, history }) => {
     return (
         <div className='r-view'>
             <PageHeader label='Exchange' onClose={() => history.push('/accounts')} />
+            <div className='r-exchange-top-wrapper'>
+                <div className='r-exchange-from-container'>
+                    a
+                </div>
+            </div>
+            <div className='r-exchange-bottom-wrapper'>
+                <div className='r-exchange-rate-container'>a</div>
+                <div className='r-exchange-to-container'>a</div>
+                <div className='r-exchange-button-container'>a</div>
+            </div>
         </div>
     )
 }
