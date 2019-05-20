b26a1a0ffacd7217bc0f2ec0d377fde0889ada3b
Soley Pasban <pasban110@gmail.com>
2019-05-20 13:26:15 +0300

    auto

--- src/server.js
+++ src/server.js
@@ -2,8 +2,10 @@ import fetch from 'isomorphic-unfetch'
 import convert from 'xml-js'
 
 export const getEuropa = async (callback) => {
-    // const xml = await fetch('https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml')
-    const xml = await fetch('http://localhost:3000/test.xml')
+    const isLive = window && (window.location.host.indexOf('localhost') < 0)
+    // const url = isLive ? 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml' : 'http://localhost:3000/test.xml'
+    const url = isLive ? '//live.php' : 'http://localhost:3000/test.xml'
+    const xml = await fetch(url)
     const raw = await xml.text()
     const json = JSON.parse(convert.xml2json(raw, { compact: true, spaces: 4 }));
     const rate = {}
