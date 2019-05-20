a0c3d17623d6920f5770ee58ec9c3164b74f5833
Soley Pasban <pasban110@gmail.com>
2019-05-20 20:17:55 +0300

    refactoring about view

--- src/views/AboutView.js
+++ src/views/AboutView.js
@@ -4,18 +4,15 @@ import { PageHeader } from '../components/PageHeader';
 import soleyPicture from '../assets/soley.jpg';
 import { Footerbar } from '../components/Footerbar';
 
-const AboutView = () => {
-    return (
-        <div className='r-view'>
-            <PageHeader label='About' onClose={() => browseTo('/accounts')} />
-            <div className='r-about-gradient' />
-            <img className='r-about-icon' src={soleyPicture} alt='' />
-            <div className='r-about-name'>Soley Pasban</div>
-            <div className='r-about-email'>pasban110@gmail.com</div>
+const AboutView = () =>
+    <div className='r-view'>
+        <PageHeader label='About' onClose={() => browseTo('/accounts')} />
+        <div className='r-about-gradient' />
+        <img className='r-about-icon' src={soleyPicture} alt='' />
+        <div className='r-about-name'>Soley Pasban</div>
+        <div className='r-about-email'>pasban110@gmail.com</div>
 
-            <Footerbar />
-        </div>
-    )
-}
+        <Footerbar />
+    </div>
 
 export { AboutView };
