6f9fa5288e42ad303d0cba8821d1d427e4831882
Soley Pasban <pasban110@gmail.com>
2019-05-20 13:38:47 +0300

    deploy script updated

--- go.bat
+++ go.bat
@@ -1,2 +1,2 @@
-REM cls && del /Q /S deploy\* && yarn build && xcopy /y/E build\* deploy\* && move deploy\index.html deploy\index.php && del deploy\test.xml && cd deploy && git add . && git commit -am "auto" && git push heroku master && cd..
-git add . && git commit -am "auto" && git push heroku master && cd..
+cls && yarn build && xcopy /y/E build\* deploy\* && move deploy\index.html deploy\index.php && del deploy\test.xml && cd deploy && git add . && git commit -am "auto" && git push heroku master && cd..
+REM cd deploy && git add . && git commit -am "auto" && git push heroku master && cd..
