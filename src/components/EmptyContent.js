8f86a201e5941a38ef173dd4149eb910a0288124
Soley Pasban <pasban110@gmail.com>
2019-05-20 12:04:18 +0300

    empty content added

---
+++ src/components/EmptyContent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Icons } from '../images/Icons';
+import { browseTo } from '../dictionary/History';
+
+const EmptyContent = ({ }) =>
+    <div className='r-exmpty-wrapper'>
+        There is nothing here!
+    </div>
+
+export { EmptyContent };
