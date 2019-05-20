712e9d75229287a9328bf80a4429303ca80c3ffc
Soley Pasban <pasban110@gmail.com>
2019-05-20 13:38:38 +0300

    fetch rate server files updated

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import convert from 'xml-js'
 export const getEuropa = async (callback) => {
     const isLive = window && (window.location.host.indexOf('localhost') < 0)
     // const url = isLive ? 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml' : 'http://localhost:3000/test.xml'
-    const url = isLive ? '//live.php' : 'http://localhost:3000/test.xml'
+    const url = isLive ? 'https://soley-revolut.herokuapp.com/rates.php' : 'http://localhost:3000/test.xml'
     const xml = await fetch(url)
     const raw = await xml.text()
     const json = JSON.parse(convert.xml2json(raw, { compact: true, spaces: 4 }));
