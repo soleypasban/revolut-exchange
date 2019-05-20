d85a412e352262620b161c653c2d1cbb09372527
Soley Pasban <pasban110@gmail.com>
2019-05-20 12:44:10 +0300

    footerbar shows active view

--- src/components/Footerbar.js
+++ src/components/Footerbar.js
@@ -2,19 +2,25 @@ import React from 'react';
 import { Icons } from '../images/Icons';
 import { browseTo } from '../dictionary/History';
 
+
 const Footerbar = ({ }) =>
     <div className='r-footer-wrapper'>
-        <FooterButton label='Accounts' icon='wallet' onClick={() => browseTo('/accounts')} />
-        <FooterButton label='Rates' icon='stock' onClick={() => browseTo('/rates')} />
-        <FooterButton label='Exchange' icon='exchange2' onClick={() => browseTo('/exchange')} />
-        <FooterButton label='About' icon='info' onClick={() => browseTo('/about')} />
-        <FooterButton label='Logout' icon='logout' onClick={() => browseTo('/')} />
+        <FooterButton label='Accounts' icon='wallet' route='/accounts' />
+        <FooterButton label='Rates' icon='stock' route='/rates' />
+        <FooterButton label='Exchange' icon='exchange2' route='/exchange' />
+        <FooterButton label='About' icon='info' route='/about' />
+        <FooterButton label='Logout' icon='logout' route='/' />
     </div>
 
-const FooterButton = ({ label, icon, onClick }) =>
-    <div className='r-footer-button' onClick={onClick}>
-        <span className='r-footer-button-icon'>{Icons[icon]}</span>
-        <span className='r-footer-button-text'>{label}</span>
-    </div>
+const FooterButton = ({ label, icon, route }) => {
+    const path = window && window.location.pathname
+    const isActive = (path === route)
+    return (
+        <div className={`r-footer-button ${isActive ? 'r-footer-button-active' : ''}`} onClick={() => browseTo(route)}>
+            <span className='r-footer-button-icon'>{Icons[icon]}</span>
+            <span className='r-footer-button-text'>{label}</span>
+        </div>
+    )
+}
 
 export { Footerbar };
