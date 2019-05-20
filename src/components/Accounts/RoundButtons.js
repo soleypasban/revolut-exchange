fca470099d8a350e0c7960cd1631493577565ad1
Soley Pasban <pasban110@gmail.com>
2019-05-20 11:27:08 +0300

    style updated

--- src/components/Accounts/RoundButtons.js
+++ src/components/Accounts/RoundButtons.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Icons } from '../../images/Icons';
 
 const RoundButtons = props =>
-    <div className='r-round-button-wrapper'>
-        <span className='r-round-button-icon' onClick={props.onClick}>{Icons[props.icon]}</span>
+    <div className='r-round-button-wrapper' onClick={props.onClick}>
+        <span className='r-round-button-icon'>{Icons[props.icon]}</span>
         <span className='r-round-button-text'>{props.label}</span>
     </div>
 
