b0a587b4edbf59862fb9c6158334b3f37c695fbd
Soley Pasban <pasban110@gmail.com>
2019-06-02 16:59:20 +0300

    proptype added to rate box

--- src/components/RateBox.js
+++ src/components/RateBox.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { stockArrow } from '../images/Icons';
 import { CurrencySign } from '../dictionary/Currencies'
+import PropTypes from 'prop-types';
 
 const RateBox = ({ from, to, rate }) =>
     <div className='r-rate-box-container'>
         {stockArrow} {CurrencySign[from]}{rate && 1}&nbsp;=&nbsp;{CurrencySign[to]}{(rate).toFixed(4)}
     </div>
+
+RateBox.propTypes = {
+    from: PropTypes.string.isRequired,
+    to: PropTypes.string.isRequired,
+    rate: PropTypes.number.isRequired,
+};
 
 export { RateBox };
